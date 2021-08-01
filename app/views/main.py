b19bca668085b3d1c9494f38142e847bc64b7078
jinse <jinseyou@gmail.com>
2021-08-01 14:03:23 +0900

    hello 추가

--- app/views/main.py
+++ app/views/main.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/')
+@bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return "Pybo index"
