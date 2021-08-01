2647c3259c7fb1765b9bc0e641776a19a03b5f85
jinse <jinseyou@gmail.com>
2021-08-01 16:03:55 +0900

    페이지 개편

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    from .views import main
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    # 블루 프린트
+    from .views import main, Question, Answer
     app.register_blueprint(main.bp)
+    app.register_blueprint(Question.bp)
+    app.register_blueprint(Answer.bp)
 
     return app
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASE_DIR}/pybo.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
